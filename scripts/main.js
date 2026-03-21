7a5075895764250f81aca4371fa88f88cb08056b
atherbeauta <kmuamba2@byupathway.edu>
2026-03-21 22:38:01 +0200

    fix2

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const hamburger = document.getElementById("hamburger-menu");
-    const navMenu = document.getElementById("nav-menu");
+    const btn = document.getElementById("hamburger-menu");
+    const menu = document.getElementById("nav-menu");
 
-    if (hamburger && navMenu) {
-        hamburger.addEventListener("click", () => {
-            navMenu.classList.toggle("open");
-            // change the hamburger icon to a close icon when the menu is open
-            hamburger.innerHTML = navMenu.classList.contains("open") ? "&#10006;" : "&#9776;";
+    if (btn && menu) {
+        btn.addEventListener("click", () => {
+            menu.classList.toggle("open");
+            // Change l'icône entre ☰ et ✕
+            btn.innerHTML = menu.classList.contains("open") ? "&#10006;" : "&#9776;";
         });
-    }
-
-    // Update automaticly the year in the footer
-    if (footerYear) {
-        footerYear.textContent = `© ${new Date().getFullYear()} Sleep Outside - Kazadi A. Muamba`;
     }
 });
